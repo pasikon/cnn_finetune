654c303f62a29e2b4354a5971d9473d4b14d94f2
michalz <michal.zurek7@gmail.com>
2017-12-14 09:27:26 +0100

    more fixes 3

--- data_load_memory.py
+++ data_load_memory.py
@@ -3,7 +3,20 @@ from subprocess import check_output
 import cv2
 import numpy as np
 import pandas as pd
+from keras.utils.np_utils import to_categorical
 from PIL import Image
+
+
+def load_problematic_pics():
+    print('Loading set of problematic pics....')
+    imgli = [cv2.imread('seedlings_data/test/00d090cde.png'), cv2.imread('seedlings_data/test/558aa7deb.png'),
+             cv2.imread('seedlings_data/test/6a47821f9.png'), cv2.imread('seedlings_data/test/cc74feadc.png')]
+    imgli = [cv2.resize(img, (300, 300), interpolation=cv2.INTER_AREA) for img in imgli]
+
+    y = np.array([0, 0, 0, 6])
+    y = to_categorical(y, 12)
+
+    return np.array(imgli), y
 
 
 def load_train_data_dir(data_root_dir, train_dir):
@@ -34,13 +47,11 @@ def load_train_data_dir(data_root_dir, train_dir):
             im_height.append(im.height)
             im_width.append(im.width)
 
-    df_all = pd.DataFrame({"class": im_class, "height": im_height, "width": im_width}, index=images)
+    # df_all = pd.DataFrame({"class": im_class, "height": im_height, "width": im_width}, index=images)
 
     df_by_class = pd.DataFrame(
         {"file": images, "class": im_class, "height": im_height, "width": im_width, "path": pathes},
         index=pathes)
-
-    df_by_class
 
     one_hot_by_class = pd.get_dummies(df_by_class['class'])
 
--- inc_finetune.py
+++ inc_finetune.py
@@ -1,12 +1,29 @@
 import numpy as np
+import pathlib
 from keras.applications.inception_v3 import InceptionV3
 from keras.models import Model
-from keras.layers import Dense, GlobalAveragePooling2D, Input
+from keras.layers import Dense, GlobalAveragePooling2D, Input, Dropout
 from sklearn.model_selection import train_test_split
-from k_fold_with_model_poc import get_datagen_augment, get_img_dim, get_datagen_noop, ld_test_set
+from k_fold_with_model_poc import get_datagen_augment, get_img_dim, ld_test_set
 from keras.optimizers import SGD, Adam
 from keras.layers.normalization import BatchNormalization
-from data_load_memory import load_train_data_dir
+from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau, CSVLogger, TensorBoard, Callback, LambdaCallback
+from data_load_memory import load_train_data_dir, load_problematic_pics
+from time import gmtime, strftime
+
+
+class ProblematicPicsCallback(Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        x, y = self.test_data
+        loss, acc = self.model.evaluate(x, y, verbose=0)
+        print('\nTesting PROBLEMATIC PICS loss: {}, acc: {}\n'.format(loss, acc))
+
+
+problematic_pics, problematic_pics_true = load_problematic_pics()
+problematic_pics = problematic_pics / 255.
 
 x_loaded = np.load('seedlings_data/numpy_imgs_resized_train.npy')
 x_loaded = np.array(x_loaded, np.float32) / 255.
@@ -19,6 +36,9 @@ x_train, x_test, y_train, y_test = train_test_split(x_loaded, y_loaded, test_siz
 batch_size = 32
 train_steps = len(x_train) / batch_size
 valid_steps = len(x_test) / batch_size
+run_date_time_str = strftime("run_%Y-%m-%d-%H_%M_%S", gmtime())
+pathlib.Path('FINETUNING/' + run_date_time_str).mkdir(parents=True, exist_ok=True)
+
 
 # create the base pre-trained model
 def inceptionv3_model():
@@ -31,7 +51,9 @@ def inceptionv3_model():
     x = GlobalAveragePooling2D()(x)
     # let's add a fully-connected layer
     x = Dense(512, activation='relu')(x)
+    x = Dropout(0.4)(x)
     x = Dense(256, activation='relu')(x)
+    x = Dropout(0.2)(x)
     # and a logistic layer
     predictions = Dense(12, activation='softmax')(x)
 
@@ -48,13 +70,13 @@ for layer in base_model.layers:
     layer.trainable = False
 
 # compile the model (should be done *after* setting layers to non-trainable)
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
 model.summary()
 
 # train the model on the new data for a few epochs
 model.fit_generator(generator=get_datagen_augment().flow(x=x_train, y=y_train, batch_size=batch_size),
-                    steps_per_epoch=train_steps, epochs=10, validation_data=(x_test, y_test), workers=12,
+                    steps_per_epoch=train_steps, epochs=6, validation_data=(x_test, y_test), workers=12,
                     max_queue_size=100)
 
 # at this point, the top layers are well trained and we can start fine-tuning
@@ -69,8 +91,8 @@ for i, layer in enumerate(base_model.layers):
 # we chose to train the top 2 inception blocks, i.e. we will freeze
 # the first 249 layers and unfreeze the rest:
 for layer in base_model.layers[:249]:
-    layer.trainable = False
-    # layer.trainable = True
+    # layer.trainable = False
+    layer.trainable = True
 for layer in base_model.layers[249:]:
     layer.trainable = True
 
@@ -78,14 +100,25 @@ model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metric
 
 model.summary()
 
+callbacks = [
+    ModelCheckpoint(filepath='FINETUNING/' + run_date_time_str + '/weights.{epoch:02d}-loss_{val_loss:.5f}.hdf5',
+                    verbose=1, save_best_only=True, save_weights_only=True, mode='auto'),
+    ProblematicPicsCallback((problematic_pics, problematic_pics_true)),
+    TensorBoard(log_dir='FINETUNING/' + run_date_time_str),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=2, cooldown=1, verbose=1),
+    CSVLogger('FINETUNING/' + run_date_time_str + '/training_log.csv')
+]
+
 # train model again (this time fine-tuning the top 2 inception blocks
 # alongside the top Dense layers
 model.fit_generator(generator=get_datagen_augment().flow(x=x_train, y=y_train, batch_size=batch_size),
-                    steps_per_epoch=train_steps, epochs=20, validation_data=(x_test, y_test), workers=12,
+                    steps_per_epoch=train_steps, epochs=80, callbacks=callbacks,
+                    validation_data=(x_test, y_test), workers=12,
                     max_queue_size=100)
 
 score_test = model.evaluate(x=x_test, y=y_test, verbose=0)
 score_train = model.evaluate(x=x_train, y=y_train, verbose=0)
+score_problematic_pics = model.evaluate(x=problematic_pics, y=problematic_pics_true, verbose=0)
 score_train_aug = model.evaluate_generator(generator=get_datagen_augment().flow(x_train, y_train, batch_size,
                                                                                 shuffle=False),
                                            steps=train_steps, max_queue_size=100, workers=12)
@@ -97,6 +130,7 @@ print('Train Score: {}'.format(score_train))
 print('Test Score: {}'.format(score_test))
 print('Train Score AUG: {}'.format(score_train_aug))
 print('Test Score AUG: {}'.format(score_test_aug))
+print('Problematic pics score: {}'.format(score_problematic_pics))
 
 print('Loading competition set...')
 c_set = ld_test_set()
